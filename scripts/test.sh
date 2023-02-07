465d6f0d700c915ce0becf3e08f3bf44e8a4f96c
PeteRager <76050312+PeteRager@users.noreply.github.com>
2023-02-07 16:07:06 -0500

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,22 @@
 #/bin/bash -xv
+
+# change these entries 
 HASSNAME="test"
 TARGET="/mnt/c/github/ha_test"
-source ./module_scripts.sh
 
+# do not change
+source ./module_scripts.sh
 build_init
 
+# add your entities here and replace the examples
 # domain, entity, timeout in seconds
 build_switch switch_1 10
 build_climate thermostat_1 5
 build_fan fan_1 5
 
+# remove this unless you are trying this in simulation mode
 cp rs_simulation_package.yaml $TARGET/packages/
 
+# do not remove
 build_finish
 
